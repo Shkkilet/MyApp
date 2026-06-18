be0ff753d4e38c722fb61bd3db2b91c92c9a731b
Shkkilet <mguk704@gmail.com>
2026-06-18 08:07:59 +0300

    added new class to Core

--- MyApp.Core/Class1.cs
+++ MyApp.Core/Class1.cs
@@ -1,6 +1,34 @@
 ﻿namespace MyApp.Core;
 
-public class Class1
+public class ProductCatalog
 {
+    private readonly List<string> _products = new();
 
+    public void Add(string name)
+    {
+        _products.Add(name);
+    }
+
+    public void Remove(string name)
+    {
+        _products.Remove(name);
+    }
+
+    public IEnumerable<string> GetAll()
+    {
+        return _products;
+    }
+
+    public string? Find(string name)
+    {
+        return _products.FirstOrDefault(x => x == name);
+    }
+
+    public string? Find(int index)
+    {
+        if (index < 0 || index >= _products.Count)
+            return null;
+
+        return _products[index];
+    }
 }
