591ef6c102d848d102cf1bd66a4695cfe8d8fe39
Shkkilet <mguk704@gmail.com>
2026-06-22 18:44:02 +0300

    added 2 classes to the Project In the Person class user getters and custom setters In the BankAccount class worked with acces modifiers

---
+++ MyApp.Core/Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyApp.Core
+{
+    public class Person
+    {
+        public string Name { get; init; }
+
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            set {
+                if (value < 0)
+                    throw new ArgumentException("Age cannot be negative");
+
+                age = value;
+            }
+        }
+    }
+}
