145807f13072d1c6394e25602acc1012253b49eb
Shkkilet <mguk704@gmail.com>
2026-06-25 11:34:31 +0300

    completed third task by using a Dictionary and LINQ completed a fourth task with a hashset

--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -1,116 +1,152 @@
 ﻿using MyApp.Core;
+using System.Text.RegularExpressions;
 
-var catalog = new ProductCatalog();
-Console.WriteLine("""
-Команди:
-add,
-remove,
-list,
-findname,
-findindex,
-exit,
-""");
-while (true)
-{
-   
-    Console.Write("Команда: ");
-    var command = Console.ReadLine();
+//var catalog = new ProductCatalog();
+//Console.WriteLine("""
+//Команди:
+//add,
+//remove,
+//list,
+//findname,
+//findindex,
+//exit,
+//""");
+//while (true)
+//{
 
-    switch (command)
-    {
-        case "add":
+//    Console.Write("Команда: ");
+//    var command = Console.ReadLine();
 
-            Console.Write("Iм'я продукту: ");
+//    switch (command)
+//    {
+//        case "add":
 
-            var product = Console.ReadLine();
+//            Console.Write("Iм'я продукту: ");
 
-            if (!string.IsNullOrWhiteSpace(product))
-            {
-                catalog.Add(product);
-            }
+//            var product = Console.ReadLine();
 
-            break;
+//            if (!string.IsNullOrWhiteSpace(product))
+//            {
+//                catalog.Add(product);
+//            }
 
-        case "remove":
+//            break;
 
-            Console.Write("Iм'я продукту: ");
+//        case "remove":
 
-            var removeProduct = Console.ReadLine();
+//            Console.Write("Iм'я продукту: ");
 
-            if (!string.IsNullOrWhiteSpace(removeProduct))
-            {
-                if (catalog.Find(removeProduct) is null)
-                {
-                    Console.WriteLine("Товар не знайдено");
-                    break;
-                }
-                else
-                {
-                    catalog.Remove(removeProduct);
-                }
-            }
+//            var removeProduct = Console.ReadLine();
 
-            break;
+//            if (!string.IsNullOrWhiteSpace(removeProduct))
+//            {
+//                if (catalog.Find(removeProduct) is null)
+//                {
+//                    Console.WriteLine("Товар не знайдено");
+//                    break;
+//                }
+//                else
+//                {
+//                    catalog.Remove(removeProduct);
+//                }
+//            }
 
-        case "list":
+//            break;
 
-            foreach (var item in catalog.GetAll())
-            {
-                Console.WriteLine(item);
-            }
+//        case "list":
 
-            break;
+//            foreach (var item in catalog.GetAll())
+//            {
+//                Console.WriteLine(item);
+//            }
 
-        case "findname":
+//            break;
 
-            Console.Write("Iм'я продукту: ");
+//        case "findname":
 
-            var productName = Console.ReadLine();
+//            Console.Write("Iм'я продукту: ");
 
-            if (!string.IsNullOrWhiteSpace(productName))
-            {
-                var result = catalog.Find(productName);
+//            var productName = Console.ReadLine();
 
-                if (result is not null)
-                {
-                    Console.WriteLine($"Знайдено: {result}");
-                }
-                else
-                {
-                    Console.WriteLine("Товар не знайдено");
-                }
-            }
+//            if (!string.IsNullOrWhiteSpace(productName))
+//            {
+//                var result = catalog.Find(productName);
 
-            break;
+//                if (result is not null)
+//                {
+//                    Console.WriteLine($"Знайдено: {result}");
+//                }
+//                else
+//                {
+//                    Console.WriteLine("Товар не знайдено");
+//                }
+//            }
 
-        case "findindex":
+//            break;
 
-            Console.Write("Iндекс: ");
+//        case "findindex":
 
-            if (int.TryParse(Console.ReadLine(), out int index))
-            {
-                var result = catalog.Find(index);
+//            Console.Write("Iндекс: ");
 
-                if (result is not null)
-                {
-                    Console.WriteLine($"Знайдено: {result}");
-                }
-                else
-                {
-                    Console.WriteLine("Неправильний iндекс");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Iндекс повинен бути числом");
-            }
+//            if (int.TryParse(Console.ReadLine(), out int index))
+//            {
+//                var result = catalog.Find(index);
 
-            break;
-        case "exit":
-            return;
+//                if (result is not null)
+//                {
+//                    Console.WriteLine($"Знайдено: {result}");
+//                }
+//                else
+//                {
+//                    Console.WriteLine("Неправильний iндекс");
+//                }
+//            }
+//            else
+//            {
+//                Console.WriteLine("Iндекс повинен бути числом");
+//            }
 
-        default:
-            Console.WriteLine("Незнайома команда");
-            break;
-    }
-}
+//            break;
+//        case "exit":
+//            return;
+
+//        default:
+//            Console.WriteLine("Незнайома команда");
+//            break;
+//    }
+//}
+
+//Завдання 3
+string text = "apple banana apple cherry banana apple";
+var res = text.Split(' ')
+    .GroupBy(x => x)
+    .ToDictionary(g => g.Key, g => g.Count())
+    .OrderByDescending(x => x.Value);
+
+foreach (var x in res)
+    Console.WriteLine($"{x.Key}={x.Value}");
+
+//Завдання 4
+var hash1 = new HashSet<string> { "Stepan", "Andrii", "Serhiy", "Vasil" };
+var hash2 = new HashSet<string> { "Andrii", "Serhiy", "Katya", "Alina" };
+
+var both = new HashSet<string>(hash1);
+both.IntersectWith(hash2);
+
+Console.WriteLine("In both groups:");
+foreach (var s in both)
+    Console.WriteLine(s);
+
+var onlyFirst = new HashSet<string>(hash1);
+onlyFirst.ExceptWith(hash2);
+
+Console.WriteLine("Only in first group:");
+foreach (var s in onlyFirst)
+    Console.WriteLine(s);
+
+var all = new HashSet<string>(hash1);
+all.UnionWith(hash2);
+
+Console.WriteLine("All unique students:");
+foreach (var s in all)
+    Console.WriteLine(s);
